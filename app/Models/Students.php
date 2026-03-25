a9dee6b2c35439e5ef000458c23463ecbfa0ddcc
Daniel <danielalejandro.2o43@gmail.com>
2026-03-25 00:54:33 -0600

    feature/new model student

---
+++ app/Models/Students.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Students extends Model
+{
+    use HasFactory; //sirve para conectar un modelo con su respectiva fabrica
+
+    protected $filleable = [
+        'first_name', 
+        'last_name', 
+        'email', 
+        'phone', 
+        'status'
+    ];
+
+    protected $casts = [ //los atributos tienen que ser convertidos a tipos especificos, convierte status a un valor consistente
+        'status'=>'string' 
+    ];
+
+}
