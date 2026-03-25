1b1c139e4041823f31d8ad67185694ed90ce0aa6
ingvegambv <ingjulianvega1@gmail.com>
2026-03-25 01:49:52 -0600

    feature/ create new rute

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\StudentsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::apiResource('students', StudentsController::class);
